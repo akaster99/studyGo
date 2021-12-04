2a2abcac522789b0e3ae5767459d7dda45ec07a1
akaster99 <idavid29@naver.com>
2021-12-05 06:44:33 +0900

    3-0

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/akaster99/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request Failed")
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "First Word"}
-	error := dictionary.Add("hello", "greeting")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println("Add Success")
-	}
-	error = dictionary.Update("hello", "hello is greeting")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println("Update Success")
-	}
-	error = dictionary.Delete("hello")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println("Delete Success")
-	}
-	definition, error := dictionary.Search("hello")
-	if error != nil {
-		fmt.Println(error)
-	} else {
-		fmt.Println(definition)
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://www.google.com",
+		"https://soundcloud.com",
+		"https://www.facebook.com",
+		"https://www.instargam.com",
+		"https://academy.nomadcoder.co/",
 	}
 
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("checking", url)
+	resp, error := http.Get(url)
+	if error != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
