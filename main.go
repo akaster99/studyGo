070515b4b8f953a7adf66c469505353dcc1bfc2f
akaster99 <idavid29@naver.com>
2021-12-05 06:33:31 +0900

    2-6

--- main.go
+++ main.go
@@ -12,7 +12,19 @@ func main() {
 	if error != nil {
 		fmt.Println(error)
 	} else {
-		fmt.Println("Success")
+		fmt.Println("Add Success")
+	}
+	error = dictionary.Update("hello", "hello is greeting")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println("Update Success")
+	}
+	error = dictionary.Delete("hello")
+	if error != nil {
+		fmt.Println(error)
+	} else {
+		fmt.Println("Delete Success")
 	}
 	definition, error := dictionary.Search("hello")
 	if error != nil {
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,7 +5,9 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Word is not found")
+var errCantUpdate = errors.New("Cant update non existing word")
 var errWordExists = errors.New("Word already exists")
+var errCantDelete = errors.New("Cant delete non existing word")
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -25,4 +27,27 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		return errCantUpdate
+	case nil:
+		d[word] = definition
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		return errCantDelete
+	case nil:
+		delete(d, word)
+	}
+	return nil
+
 }
