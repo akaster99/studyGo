8acde370c309ed0ca2f60614ea0bc68169996012
akaster99 <idavid29@naver.com>
2021-12-05 07:03:29 +0900

    3-2

--- main.go
+++ main.go
@@ -4,34 +4,37 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-	urls := []string{
-		"https://www.airbnb.com",
-		"https://www.google.com",
-		"https://www.amazon.com",
-		"https://www.reddit.com",
-		"https://www.google.com",
-		"https://soundcloud.com",
-		"https://www.facebook.com",
-		"https://www.instargam.com",
-		"https://academy.nomadcoder.co/",
-	}
-	results := make(map[string]string)
-	for _, url := range urls {
-		error := hitURL(url)
-		if error != nil {
-			results[url] = "fail"
-		} else {
-			results[url] = "success"
-		}
-	}
-	for url, result := range results {
-		fmt.Println(url, ": ", result)
-	}
+	// urls := []string{
+	// 	"https://www.airbnb.com",
+	// 	"https://www.google.com",
+	// 	"https://www.amazon.com",
+	// 	"https://www.reddit.com",
+	// 	"https://www.google.com",
+	// 	"https://soundcloud.com",
+	// 	"https://www.facebook.com",
+	// 	"https://www.instargam.com",
+	// 	"https://academy.nomadcoder.co/",
+	// }
+	// results := make(map[string]string)
+	// for _, url := range urls {
+	// 	error := hitURL(url)
+	// 	if error != nil {
+	// 		results[url] = "fail"
+	// 	} else {
+	// 		results[url] = "success"
+	// 	}
+	// }
+	// for url, result := range results {
+	// 	fmt.Println(url, ": ", result)
+	// }
+	go sexyCount("akaster99")
+	sexyCount("idavid29")
 }
 
 func hitURL(url string) error {
@@ -40,4 +43,11 @@ func hitURL(url string) error {
 		return errRequestFailed
 	}
 	return nil
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
 }
