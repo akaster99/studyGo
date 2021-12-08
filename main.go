bf96fdde66e5f52261d8b206cb53728cb569ec1b
akaster99 <idavid29@naver.com>
2021-12-09 04:48:27 +0900

    3-7

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-type result struct {
+type requestResult struct {
 	url    string
 	status string
 }
@@ -14,7 +14,7 @@ type result struct {
 var errRequestFailed = errors.New("Request Failed")
 
 func main() {
-	c := make(chan result)
+	c := make(chan requestResult)
 	urls := []string{
 		"https://www.airbnb.com",
 		"https://www.google.com",
@@ -30,14 +30,20 @@ func main() {
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, ":", status)
+	}
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, error := http.Get(url)
 	status := "OK"
 	if error != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	c <- result{url: url, status: status}
+	c <- requestResult{url: url, status: status}
 }
